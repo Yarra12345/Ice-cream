cb8b9d80ec81a6d834df3c351a3cc9ff8110971a
Yaroslav <y.chursin@inbox.ru>
2021-05-13 18:54:56 +0600

    На всякий случай

--- js.js
+++ js.js
@@ -1,11 +1,23 @@
+let menustatus="off"
+
+function hidden_the_menu_k() {
+    setTimeout(() => {
+        if( menustatus==="off") {
+        document.getElementById("menu").style.visibility="hidden"
+        }
+    }, 500);
+        menustatus="off"
+}
+
 function show_the_menu() {
+    menustatus="on"
     let div = document.getElementById("menu") ;
     div.style.visibility="visible"
 }
-
 function hidden_the_menu() {
-    let div = document.getElementById("menu") ;
-    div.style.visibility="hidden"
+        let div = document.getElementById("menu") ;
+        div.style.visibility="hidden"
+        menustatus="off"
 }
 
 function showOrHideElement(id) {
@@ -24,8 +36,8 @@ function shar1() {
     document.body.style.backgroundColor="#849d8f";
     let section = document.getElementById("creams_section");
     section.style.background="radial-gradient(#ffffff 0% , #849d8f 70%)";
-    // Element.scr="./img/ice one.png"
-    // Element.scr="./img/ice two.png"
+    document.getElementById("creamform").src="./img/ice one.png";
+    document.getElementById("creamsform").src="./img/ice two.png";
 }
 
 function shar2() {
@@ -34,8 +46,8 @@ function shar2() {
     document.body.style.backgroundColor="#8996a6";
     let section = document.getElementById("creams_section");
     section.style.background="radial-gradient(#ffffff 0% , #8996a6 70%)";
-    // Element.scr="./img/ice one_secondform.png"
-    // Element.scr="./img/ice two_secondform.png"
+    document.getElementById("creamform").src="./img/ice one_secondform.png";
+    document.getElementById("creamsform").src="./img/ice two_secondform.png";
 }
 
 function shar3() {
@@ -44,6 +56,11 @@ function shar3() {
     document.body.style.backgroundColor="#9d8b84";
     let section = document.getElementById("creams_section");
     section.style.background="radial-gradient(#ffffff 0% , #9d8b84 70%)";
-    // Element.scr="./img/ice one_thirdform.png"
-    // Element.scr="./img/ice two_thirdform.png"
+    document.getElementById("creamform").src="./img/ice one_thirdform.png";
+    document.getElementById("creamsform").src="./img/ice two_thirdform.png";
+}
+
+function xxx(id) {
+    let div = document.getElementById(id)
+    div.style.visibility="hidden"
 }
